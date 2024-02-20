c881616a6d4721fbc814c1f2d0aa033bef228d20
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-02-20 11:07:04 -0600

    Add files via upload

---
+++ funkmusic.py
@@ -0,0 +1,14 @@
+
+def square(side):
+    area = side * side
+    print("the area of the square is" ,area, "square units")
+
+square(4)
+
+
+def circle(radius):
+    cirarea = 3.14 * radius
+    print("the area of the circle is" ,cirarea, "square units")
+
+circle(6)
+         
