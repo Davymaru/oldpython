b7fbe40d58a9590172fc1bce8e1e31c365eadf15
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-02-26 22:58:13 -0600

    Update madlib.py

--- current work/madlibdemo.py
+++ current work/madlibdemo.py
@@ -1,16 +1,33 @@
-def madlibs(person, animal, size, color, noun):
-    # my madllibs
-    print(f"{person} had a   {size} {animal} ")
-    print(f"Whose fleece was {color} as {noun}")
+def madlibs(object, verb, function, inanimate, noun,action, destroy, name, ):
 
 
 
 
 
-person = input("Please enter the name of a person:  ")
-size = input("Please enter a descriptive word for size:  ")
-animal = input("Please enter the kind of animal:  ")
-color = input("Please enter a color:  ")
-noun = input("Please enter a noun")
+    print(f"{noun} of {object}, Im {action} your {inanimate} ")
+    print(f"{function} Your mind and {destroy} your dreams")
+    print(f"Blinded by me you cant see a thing")
+    print(f"just call my {name} and ill hear you{verb} ")
+    print(f"{noun}! {noun}!")
 
-madlibs(person, animal, size, color, noun)
+
+
+
+
+
+name = input("please enter a different word for name here:")
+destroy = input("please enter a different word for destroying:")
+action = input("Please enter an action:")
+object = input("Please enter the name of an object:  ")
+function = input("Please enter a body function:  ")
+verb = input("Please enter a verb here :  ")
+inanimate = input("Please enter an inanimate object:  ")
+noun = input("Please enter a noun:")
+
+
+
+
+
+
+
+madlibs(object, verb, function, inanimate, noun, action, destroy, name)
