60c2bfceaef599c72007b03b1e2ba47f49919107
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-03-11 17:07:37 -0500

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,31 @@
+import random
+
+def main():
+    try:
+        target_number = random.randint(1, 100)
+
+        while True:
+         
+
+            user_number = int(input("guess a number (between 1 and 100): "))
+
+            difference = abs(user_number - target_number)
+
+            if difference <= 5:
+                print("very hot")
+            elif difference <= 15:
+                print("hot")
+            elif difference <= 25:
+                print("cool")
+            else:
+                print("cold")
+
+            if user_number == target_number:
+                print("you did it! You guessed the correct number:", target_number)
+                break  
+
+    except ValueError:
+        print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
