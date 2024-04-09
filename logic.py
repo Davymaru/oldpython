66320b0d1ab9b08fab17f43424f48c8a3476996d
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-04-09 11:03:26 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -8,10 +8,11 @@
 
 class Dog:
     # dog is a Super class of the Herding_Dog class
-    def __init__(self, color, coat, size):
+    def __init__(self, color, coat, size, name):
         self.color = color
         self.coat = coat
         self.size = size
+        self.name = name
 
     def bark(self):
         print("Woof!")
@@ -19,25 +20,39 @@ class Dog:
     def happy(self):
         print("Wag wag")
 
+    def __str__(self):
+        return f"Dog: Color: {self.color}, Coat: {self.coat}, Size: {self.size}, Name: {self.name}"
+
 
 class HerdingDog(Dog):
+    def __init__(self, color, coat, size, name, favorite_toy):
+        super().__init__(color, coat, size, name)
+        self.favorite_toy = favorite_toy
+
+    def __str__(self):
+        return super().__str__() + " " + self.favorite_toy + ", with herding skills"
+
     def herding(self):
-        print("Go over here! Get me a cookie!")
+        print("Run, I'm going to chase you.")
 
 
 def main():
 
     print("\n\n")
-    ollie = Dog("Fawn", "Short", "Large")
+    ollie = Dog("Fawn", "Short", "Large", "Ollie")
     print("Ollie: ")
     ollie.bark()
     ollie.happy()
+    print(ollie)
 
     print("\n\n")
     print("Nessie:")
-    nessie = HerdingDog("Black", "Short", "Large")
+    nessie = HerdingDog("Black", "Short", "Large",
+                        "Nessie", "Minty World Ball")
     nessie.herding()
+    print(nessie)
     print("\n\n")
 
 
 main()
+
