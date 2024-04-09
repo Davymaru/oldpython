e3657c44642bcea463aeabade1665fa7841d9de0
Davymaru <156940879+Davymaru@users.noreply.github.com>
2024-04-09 00:12:50 -0500

    Add files via upload

--- class.py
+++ class.py
@@ -1,103 +1,101 @@
+
 def main():
-    class Person:
-        def __init__(self, name, address, age, phone_number):
-            self.__name = name
 
-            self.__address = address
+    class Pet:
+        vet_name = "Your Veterinary Office"
 
-            self.__age = age
+        def __init__(self, owner_first_name, owner_last_name, pet_id, pet_name, pet_type="Dog"):
+            self.__owner_first_name = owner_first_name
 
-            self.__phone_number = phone_number
+            self.__owner_last_name = owner_last_name
 
-       
-        def get_name(self):
-            return self.__name
+            self.__pet_id = pet_id
 
+            self.__pet_name = pet_name
 
-        def get_address(self):
-            return self.__address
+            self.__pet_type = pet_type
 
 
-        def get_age(self):
-            return self.__age
+        def get_owner_first_name(self):
+            return self.__owner_first_name
+
+        def get_owner_last_name(self):
+            return self.__owner_last_name
+
+        def get_pet_id(self):
+            return self.__pet_id
+
+        def get_pet_name(self):
+            return self.__pet_name
+
+        def get_pet_type(self):
+            return self.__pet_type
 
 
-        def get_phone_number(self):
-            return self.__phone_number
+        def set_owner_first_name(self, owner_first_name):
+            self.__owner_first_name = owner_first_name
 
-        
-        
+        def set_owner_last_name(self, owner_last_name):
+            self.__owner_last_name = owner_last_name
 
+        def set_pet_id(self, pet_id):
+            self.__pet_id = pet_id
 
-        def set_name(self, name):
-            self.__name = name
+        def set_pet_name(self, pet_name):
+            self.__pet_name = pet_name
 
-        def set_address(self, address):
-            self.__address = address
+        def set_pet_type(self, pet_type):
+            self.__pet_type = pet_type
 
-        def set_age(self, age):
-            self.__age = age
+        def display_pet_info(self):
+            print("Pet Information:")
 
-        def set_phone_number(self, phone_number):
-            self.__phone_number = phone_number
+            print("Owner: {} {}".format(self.__owner_first_name, self.__owner_last_name))
 
+            print("Pet ID:", self.__pet_id)
 
+            print("Pet Name:", self.__pet_name)
 
+            print("Pet Type:", self.__pet_type)
 
+            print("Veterinary Office:", Pet.vet_name)
 
-    person1 = Person("Joe Mama", "123 Maama St, TOWNTOWN", 900000, "708-555-1234")
-
-    person2 = Person("Jar Jar Binks", "500, Gungan city", 87, "815-333-5678")
-
-    person3 = Person("Ash Ketchum", "300, Pallet town", 10, "319-444-9101")
-
-
-
+            print()
 
 
-    print("Person 1:")
-
-    print("Name:", person1.get_name())
-
-    print("Address:", person1.get_address())
-
-    print("Age:", person1.get_age())
-
-    print("Phone Number:", person1.get_phone_number())
-
-    print()
+    def check_property(pet, property_name):
+        if hasattr(pet, property_name):
+            print(f"The property '{property_name}' exists for the pet.")
+        else:
+            print(f"The property '{property_name}' does not exist for the pet.")
 
 
+    pet1 = Pet("Runnout", "Ideas", 1, "Foname")
+
+    pet2 = Pet("Pieron", "Kelley", 2, "Thunder", "Cat") #my real cats name fun fact
+
+    pet3 = Pet("Sephiroth", "One winged angel", 3, "cloud")
 
 
-    print("Person 2:")
-
-    print("Name:", person2.get_name())
-
-    print("Address:", person2.get_address())
-
-    print("Age:", person2.get_age())
-
-    print("Phone Number:", person2.get_phone_number())
-
-    print()
+    pet1.set_pet_type("Snake")
 
 
+    pet1.display_pet_info()
+
+    pet2.display_pet_info()
+
+    pet3.display_pet_info()
 
 
-    print("Person 3:")
+    print("Property Existence Check:")
 
-    print("Name:", person3.get_name())
-    
-    print("Address:", person3.get_address())
+    check_property(pet1, "get_pet_type")
 
-    print("Age:", person3.get_age())
+    check_property(pet2, "get_pet_type")
 
-    print("Phone Number:", person3.get_phone_number())
+    check_property(pet3, "get_owner_last_name")
 
-
-
-
+    check_property(pet3, "get_pet_age")
 
 
 
